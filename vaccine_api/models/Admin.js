a631a1c5ac8620d58c6006ccee1f21a61958e5c0
Suma-Geethika-Brahmakanti <129078671+Suma-Geethika-Brahmakanti@users.noreply.github.com>
2024-10-06 19:48:42 +0530

    Update Admin.js

--- vaccine_api/models/Admin.js
+++ vaccine_api/models/Admin.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const adminSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }
-    // Add any additional fields if needed
     // role: { type: String, default: 'admin' }
 });
 
