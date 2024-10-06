5726bab732e2c65c46c51191cb3c9e2e885e36c5
Suma-Geethika-Brahmakanti <129078671+Suma-Geethika-Brahmakanti@users.noreply.github.com>
2024-10-06 19:12:24 +0530

    Update populateSlots.js

--- vaccine_api/populateSlots.js
+++ vaccine_api/populateSlots.js
@@ -31,19 +31,6 @@ mongoose.connect(process.env.MONGO_URI)
             }
         }
 
-        
-        // // Generate slots for each day in the date range
-        // for (let day = startDate; day <= endDate; day.setDate(day.getDate() + 1)) {
-        //     for (let hour = 10; hour < 17; hour++) { // From 10 AM to 5 PM
-        //         for (let minute = 0; minute < 60; minute += 30) { // Every 30 minutes
-        //             const slotTime = new Date(day);
-        //             slotTime.setHours(hour, minute, 0);
-        //             // slots.push({ time: slotTime });
-        //             slots.push({ time: slotTime, remainingDoses: 10 }); // Initialize with 10 doses
-        //         }
-        //     }
-        // }
-
         // Insert the generated slots into the database
         return Slot.insertMany(slots);
     })
